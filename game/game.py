b74537c7e185996ddc54a0175d90768c22be1e05
Quinten Kent <berlinquin@gmail.com>
2022-05-17 10:20:10 -0500

    Add API stubs

---
+++ game/game.py
@@ -0,0 +1,32 @@
+# Expose this API to manage the state of the snake game
+
+def start():
+    pass
+
+
+def pause():
+    pass
+
+
+def restart():
+    pass
+
+
+def adjust_difficulty():
+    pass
+
+
+def adjust_gameboard_size():
+    pass
+
+
+def get_score():
+    pass
+
+
+def get_high_score():
+    pass
+
+
+def get_state():
+    pass
